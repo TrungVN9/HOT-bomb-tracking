93b4d3de6017052bfb120f7749270c6d80d3ead1
jshen12 <35182823+jshen12@users.noreply.github.com>
2022-02-26 14:55:20 -0800

    More post stuff

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,13 +1,46 @@
 import React, { useMemo, useState } from 'react';
-import Post from './Post';
+
+function Post(props) {
+
+  return (
+    <div className="post">
+      <div className="toprow">
+        <h3>Location: {props.location}</h3>
+      </div>
+      <p>{props.content}</p>
+    </div>
+    
+  );
+}
 
 function Feed(props) {
+
+
   
+  const [post, setPost] = useState([
+    {
+      "location": "5",
+      "content": "hi"
+    },
+    {
+      "location": "russia",
+      "content": "bye"
+    }
+  ]);
+
+  const dataToPost = Array.from(post).map((e) => {
+    return(
+      <Post
+        location={e.location}
+        content={e.content}
+      />
+    )
+  });
 
   return (
     <div className="feed">
       <h2>Feed goes here :)</h2>
-      <Post />
+      {dataToPost}
       
     </div>
     
