1a407c22a4d20729c18b70558c99170864c1f285
TrungVN9 <trungnvong956@g.ucla.edu>
2022-02-26 15:54:49 -0800

    Addded bomb-icon feature

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,22 +1,43 @@
 import React, {useState, useEffect } from 'react';
 
-import { MapContainer, TileLayer, useMap, Rectangle } from 'react-leaflet'
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from 'react-leaflet'
 
-import { Icon } from "leaflet";
-// import * as parkData from "./data/skateboard-parks.json";
+import { getIcon } from './icon'
+//import * as cityData from "./ua.json"
 
+function LocationMarker(){
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click(){
+      map.locate()
+    },
+    locationfound(e){
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+  return position === null ? null : (
+    <Marker position={position} icon={ getIcon }>
+      <Popup> Bomb is Here </Popup>
+    </Marker>
+  )
+}
 
-function Map(props) {
+function Map() {
+  const position = [50.450001, 30.523333 ]
+  //const [activeBomb, setactiveBomb] = useState(null);
   return (
-    
     <div className="map">
-      
       <h2>Map goes here :)</h2>
-      <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
+      <MapContainer 
+      center={position} 
+      zoom={13}
+      style={{ height: 500, width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
+        />
+        <LocationMarker />
       </MapContainer>
     </div>
     
---
+++ client/src/components/icon.js
@@ -0,0 +1,15 @@
+import * as L from "leaflet"
+
+const getIcon = new L.Icon({
+  iconUrl: require('../img/bombicon.jpeg'),
+  iconRetinaUrl: require('../img/bombicon.jpeg'),
+  iconAnchor: null,
+  popupAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(60, 75),
+  className: 'leaflet-div-icon'
+});
+
+export { getIcon };
