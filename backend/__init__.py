67cec7c654624bdd560f06abc5005b985bb81969
opheliasin <76908733+opheliasin@users.noreply.github.com>
2022-02-26 19:36:34 -0800

    Create sighting_scheme to convert sqlalchemy to json

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from os import path
+
 db = SQLAlchemy()
+ma = Marshmallow()
 DB_NAME = "database.db"
 
 def create_app():
@@ -9,6 +12,14 @@ def create_app():
     app.config['SECRET_KEY'] = 'hot' #store cookies
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    ma.init_app(app)
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import Sighting
+    create_database(app)
+    return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
