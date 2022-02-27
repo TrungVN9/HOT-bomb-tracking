a3415b6b1a60830b0474318cc4ca6be801e1b35c
jshen12 <35182823+jshen12@users.noreply.github.com>
2022-02-26 19:16:03 -0800

    Update Feed.js

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,16 +1,17 @@
 import React, { useMemo, useState } from 'react';
 import '../styles/Feed.css';
 
+
+const geocode_token = "pk.eyJ1IjoianNoZW4xMiIsImEiOiJjbDA0YmxkN2owOHUxM2RudHF1dTBkZjFmIn0.Sp0jaOXuPrWRgIwM-5ut9Q";
+
+
 function Post(props) {
 
   return (
     <div className="post">
-      <div className="toprow">
-        <h3>Location: {props.location}</h3>
-        <h3> </h3>
-        <h3 className='time'>Time: {props.timestamp}</h3>
-      </div>
-      <h3>{props.dangertype}</h3>
+      <h4>Location: {props.location}</h4>
+      <h4 className='time'>Time: {props.timestamp}</h4>
+      <h4>{props.dangertype}</h4>
       <p>{props.content}</p>
     </div>
     
@@ -88,6 +89,14 @@ function Feed(props) {
       "content": "bye"
     },
   ]);
+
+  const convertToAddress = async (query) => {
+    const geocode_uri = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+query[0]+","+query[1]+".json?limit=1&access_token="+geocode_token;
+    const data = await fetch(geocode_uri);
+    const object = await data.json();
+    return object.features.place_name;
+    
+  }
 
   const dataToPost = Array.from(post).map((e) => {
     return(
